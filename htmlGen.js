4b9bc263e61178d2e9eb309a96a09481ae75216e
andytheelf <ballew22@gmail.com>
2020-05-23 19:45:01 -0500

    manager card

--- htmlGen.js
+++ htmlGen.js
@@ -2,6 +2,21 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+function generateHTML(team) {
+    const createManagerCard = (manager) => `
+    <div class="card bg-info mb-3" style="max-width: 18rem;">
+        <div class="card-header">Manager</div>
+        <div class="card-body">
+            <h5 class="card-title">${manager.name}</h5>
+            <div class="card">
+                <ul class="list-group list-group-flush text-body">
+                    <li class="list-group-item">ID: ${manager.id}</li>
+                    <li class="list-group-item">Email: ${manager.email}</li>
+                    <li class="list-group-item">Office #: ${manager.officeNumber}</li>
+                </ul>
+            </div>
+        </div>
+    </div>`;
 
 
-module.exports = generateHTML;
+    module.exports = generateHTML;
